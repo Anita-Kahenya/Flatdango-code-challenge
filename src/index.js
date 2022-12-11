e8716bd241569980dce45cb49562aded69733c44
Renee Mumbi <1723renee@gmail.com>
2022-12-11 20:18:34 +0300

    first deliverable completed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 document.addEventListener("DOMContentLoaded",() => {
-    fetchingFirstMovieData();
+   // firstMovieData();
+    //fetchAllMovies();
+    fetchingFirstMovieData()
 });
 
-const baseUrl = "http://localhost:3000/films"
-const movie=document.getElementById("films")
+//const baseUrl = "http://localhost:3000/films/1"
+
  
+//1st deliverable
+//fetching the first movie data 
 function fetchingFirstMovieData(){
-    fetch(baseUrl)
+   // fetch(baseUrl)
+    fetch("http://localhost:3000/films/1")
     .then((response)=>response.json())
-    .then((data)=>{
-        console.log(data)
-    })
-}
-function fetchingFirstMovieData(data)
+    .then(data=> firstMovieData(data))
+        
+    }
+
+function firstMovieData(movieData){
+
 const poster = document.getElementById("poster")
+ poster.src=movieData.poster
 const title=document.getElementById("title")
+title.textContent=movieData.title
 const runtime=document.getElementById("runtime")
+runtime.textContent= movieData.runtime
 const showtime=document.getElementById("showtime")
+showtime.textContent=movieData.showtime
 const description=document.getElementById("film-info")
+description.textContent=movieData.description
 const availableTickets=document.getElementById("ticket-num")
+availableTickets.textContent= movieData.capacity-movieData.tickets_sold
+
+}
+
+//2nd deliverable 
+
+function fetchAllMovies(data){
+    fetch("http://localhost:3000/films")
+    .then(resp => resp.json())
+    .then(data =>data.forEach(data => moviesList(data)))
+
+}
+function moviesList(data){
+    
+}
