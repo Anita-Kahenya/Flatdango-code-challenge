999f7acc74d8568a60d161efc0768d6ca3a4324a
Renee Mumbi <1723renee@gmail.com>
2022-12-11 18:48:31 +0300

    1st deliverable variables created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// Your code here
+document.addEventListener("DOMContentLoaded",() => {
+    fetchingFirstMovieData();
+});
+
+const baseUrl = "http://localhost:3000/films"
+const movie=document.getElementById("films")
+ 
+function fetchingFirstMovieData(){
+    fetch(baseUrl)
+    .then((response)=>response.json())
+    .then((data)=>{
+        console.log(data)
+    })
+}
+function fetchingFirstMovieData(data)
+const poster = document.getElementById("poster")
+const title=document.getElementById("title")
+const runtime=document.getElementById("runtime")
+const showtime=document.getElementById("showtime")
+const description=document.getElementById("film-info")
+const availableTickets=document.getElementById("ticket-num")
